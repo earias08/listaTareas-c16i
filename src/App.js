8c470636eb5d847dd361793dea6341a261cc3db0
Emilse <emilsearias@gmail.com>
2021-11-02 10:22:44 -0300

    agregar el maquetado del formulario

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Subtitulo from "./components/Subtitulo";
 import Titulo from "./components/Titulo";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FormTareas from "./components/FormTareas";
 
 function App() {
   return (
     <>
       <Titulo ></Titulo>
       <Subtitulo comision='16i' estado={true}/>
+      <FormTareas></FormTareas>
     </>
   );
 }
