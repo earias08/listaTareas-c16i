e87f34c11df2b2d68072d1b28c81e2224c4543c3
Emilse <emilsearias@gmail.com>
2021-11-02 11:36:55 -0300

    se agrego el codigo para el evento change del formulario y en el submit

--- src/components/FormTareas.js
+++ src/components/FormTareas.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import ListaTareas from "./ListaTareas";
 
 const FormTareas = () => {
+  // aqui va la logica
+  // crear los state necesarios
+  const [tareaIndividual, setTareaIndividual] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  //   const guardarTarea = (e) =>{
+  //     //   console.log(e.target.value);
+  //       setTareaIndividual(e.target.value);
+  //   }
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('desde handleSubmit')
+      setTareas([...tareas,tareaIndividual]);
+  };
+
   return (
+    //   aqui va el maquetado y muy poca logica
     <>
-      <form className='container my-5'>
-          <div className='mb-3 d-flex'>
-            <input type="text" placeholder='Ingrese una tarea' className='form-control'/>
-            <button className='btn btn-outline-light' type='submit'>Agregar</button>
-          </div>
+      <form className="container my-5" onSubmit={handleSubmit}>
+        <div className="mb-3 d-flex">
+          <input
+            type="text"
+            placeholder="Ingrese una tarea"
+            className="form-control"
+            onChange={(e) => setTareaIndividual(e.target.value)}
+          />
+          <button className="btn btn-outline-light" type="submit">
+            Agregar
+          </button>
+        </div>
       </form>
-      <section className='container'>
+      <section className="container">
         <ListaTareas></ListaTareas>
       </section>
     </>
